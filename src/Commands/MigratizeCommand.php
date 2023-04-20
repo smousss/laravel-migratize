00a471ce341b8a561e73d0ec6aef75b114c4a3d0
Benjamin Crozat <benjamincrozat@me.com>
2023-04-20 11:41:13 +0200

    Improved error handling + clean up

--- src/Commands/MigratizeCommand.php
+++ src/Commands/MigratizeCommand.php
@@ -2,14 +2,13 @@
 
 namespace Smousss\Laravel\Migratize\Commands;
 
-use ReflectionClass;
 use Illuminate\Support\Arr;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\RequestException;
 
 class MigratizeCommand extends Command
 {
@@ -28,11 +27,11 @@ class MigratizeCommand extends Command
         $tables = $this->getTables();
 
         $choice = $this->choice('Do you want to choose the tables you need a migration for?', [
-            'Choose the files',
+            'Choose the tables',
             'Generate a migration for all the tables!',
         ], 0);
 
-        if ($choice === 'Choose the files') {
+        if ('Choose the tables' === $choice) {
             $tables = Arr::wrap(
                 $this->choice('For which table do you need a migration?', $tables->toArray(), multiple: true)
             );
@@ -41,33 +40,30 @@ class MigratizeCommand extends Command
         foreach ($tables as $table) {
             $schema = $this->getTableSchema($table);
 
-            $this->line("GPT-4 is generating the migration for {$table}…");
+            $this->line("GPT-4 is generating a migration for {$table}…");
 
-            $response = Http::withToken(config('migratize.secret_key'))
-                ->timeout(300)
-                ->retry(3)
-                ->post(config('migratize.debug', false)
-                    ? 'https://smousss.test/api/migratize'
-                    : 'https://smousss.com/api/migratize', compact('schema'))
-                ->throw()
-                ->json();
+            try {
+                $response = Http::withToken(config('migratize.secret_key'))
+                    ->timeout(300)
+                    ->retry(3)
+                    ->post(config('migratize.debug', false)
+                        ? 'https://smousss.test/api/migratize'
+                        : 'https://smousss.com/api/migratize', compact('schema'))
+                    ->throw()
+                    ->json();
 
-            File::put(base_path($path = 'database/migrations/' . date('Y_m_d_His') . "_create_{$table}_table.php"), trim(trim($response['data'], '`ph')) . PHP_EOL);
+                File::put(
+                    base_path($path = 'database/migrations/' . date('Y_m_d_His') . "_create_{$table}_table.php"),
+                    trim(trim($response['data'], '`ph')) . PHP_EOL
+                );
 
-            $this->info("Your migration has been created at $path! 🎉 (Tokens: {$response['meta']['consumed_tokens']})");
+                $this->info("Your migration has been created at $path! 🎉 (Tokens: {$response['meta']['consumed_tokens']})");
+            } catch (RequestException $e) {
+                $this->error($e->response->json()['message']);
+            }
         }
 
         return self::SUCCESS;
-    }
-
-    protected function getSourceCodeForModel(Model $model) : string
-    {
-        $sourceCode = File::get((new ReflectionClass($model::class))->getFileName());
-
-        $sourceCode = str_replace("\t", '', $sourceCode);
-        $sourceCode = str_replace("\n", ' ', $sourceCode);
-
-        return preg_replace('/ {2,}/', ' ', $sourceCode);
     }
 
     protected function getTables() : Collection
